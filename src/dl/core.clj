80abafc2cca578c069c46926adf6ab486fd60a67
Chris Oei <xcko-git@yahoo.com>
2014-10-15 21:28:57 -0700

    Add more hash fields, comment field, JSON field

--- src/dl/core.clj
+++ src/dl/core.clj
@@ -16,7 +16,7 @@
           :subname (System/getenv "OEI_DL")
         })
 
-(defn insert [r]
+(defn insert [cmd r]
   (jdbc/insert! db :dl
     {
       :uri (get-in r [:request :http-url])
@@ -25,8 +25,12 @@
       :content_type (get-in r [:headers "Content-Type"])
       :encoding (get-in r [:headers "Content-Encoding"])
       :sha2_256 (DigestUtils/sha256Hex (:body r))
+      :sha1 (DigestUtils/sha1Hex (:body r))
+      :md5 (DigestUtils/md5Hex (:body r))
       :crc32 (format "%08x" (.getValue (doto (CRC32.) (.update (:body r)))))
       :content (:body r)
+      :comment (.getOptionValue cmd "comment")
+      :j (.getOptionValue cmd "json")
       :l (count (:body r))
     })
 )
@@ -34,13 +38,26 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (insert (client/get "http://oei.io" {
-                   :as :byte-array
-                   :headers {
-                     :referer "http://oei.io"
-                     :user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36"
-                   }
-                   :save-request? true
-           })
+  (let [
+       options (doto (Options.)
+                  (.addOption "referrer" true "Referring web page")
+                  (.addOption "uri" true "URI to download")
+                  (.addOption "comment" true "Comment")
+                  (.addOption "json" true "Additional JSON")
+                )
+       argv (into-array args)
+       parser (GnuParser.)
+       cmd (.parse parser options argv)
+     ]
+    (insert cmd
+            (client/get (.getOptionValue cmd "uri") {
+                     :as :byte-array
+                     :headers {
+                       :referer (.getOptionValue cmd "referrer")
+                       :user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36"
+                     }
+                     :save-request? true
+             })
+    )
   )
 )
