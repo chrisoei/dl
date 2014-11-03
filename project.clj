c70d0b0fe23794c97a4121cd39aad11d681f0b1d
Chris Oei <xcko-git@yahoo.com>
2014-11-02 18:35:02 -0800

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
   :dependencies [
                   [org.clojure/clojure "1.6.0"]
                   [org.clojure/data.json "0.2.5"]
-                  [clj-http "1.0.0"]
+                  [clj-http "1.0.1"]
                   [commons-cli/commons-cli "1.2"]
                   [commons-codec/commons-codec "1.9"]
                   [commons-io/commons-io "2.4"]
                   [org.bouncycastle/bcprov-jdk15on "1.51"]
-                  [org.clojure/java.jdbc "0.3.5"]
+                  [org.clojure/java.jdbc "0.3.6"]
                   [org.xerial/sqlite-jdbc "3.8.7"]
                 ]
   :main ^:skip-aot dl.core
